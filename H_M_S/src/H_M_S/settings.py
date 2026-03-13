296aad4867ad7b96a67e1f560fb53aa5ac46af0f
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-13 17:10:39 +0200

    Adding projects apps

--- H_M_S/src/H_M_S/settings.py
+++ H_M_S/src/H_M_S/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My_Apps
+    'accounts',
+    'patients',
+    'doctors',
+    'billing',
+    'appointments',
 ]
 
 MIDDLEWARE = [
