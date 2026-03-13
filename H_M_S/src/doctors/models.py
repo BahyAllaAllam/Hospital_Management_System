56ac70d4679b1c52c30746725a692f02a289a3d2
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-13 17:26:16 +0200

    Create doctors models

--- H_M_S/src/doctors/models.py
+++ H_M_S/src/doctors/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from accounts.models import User
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    specialization = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=50, unique=True)
+    consultation_fee = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"Dr. {self.user.get_full_name()} - {self.specialization}"
