8056b75ab0a3a9cb2ae3df8e86c8070518b4e2c6
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-13 17:19:11 +0200

    Create accounts app models

--- H_M_S/src/accounts/models.py
+++ H_M_S/src/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLES = (
+        ('admin', 'Admin'),
+        ('doctor', 'Doctor'),
+        ('nurse', 'Nurse'),
+        ('receptionist', 'Receptionist'),
+        ('patient', 'Patient'),
+    )
+    role = models.CharField(max_length=20, choices=ROLES)
+    phone = models.CharField(max_length=15, blank=True)
