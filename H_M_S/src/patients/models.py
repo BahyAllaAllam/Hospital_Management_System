3c5b10df125cf33fc32718c4be55a19d0ddd77aa
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-13 17:23:23 +0200

    Create patients models

--- H_M_S/src/patients/models.py
+++ H_M_S/src/patients/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from accounts.models import User
+
+class Patient(models.Model):
+    BLOOD_TYPES = [
+        ('A+', 'A+'), ('A-', 'A-'),
+        ('B+', 'B+'), ('B-', 'B-'),
+        ('O+', 'O+'), ('O-', 'O-'),
+        ('AB+', 'AB+'), ('AB-', 'AB-'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    blood_type = models.CharField(max_length=3, choices=BLOOD_TYPES)
+    address = models.TextField()
+    emergency_contact_name = models.CharField(max_length=100)
+    emergency_contact_phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.user.get_full_name()} - {self.blood_type}"
