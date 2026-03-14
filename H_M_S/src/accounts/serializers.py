0a521548e80acef11477e1f4ff91fe2a26ff58be
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-14 17:30:27 +0200

    Adding the Rest_Frame and creating the serializers for the accounts

---
+++ H_M_S/src/accounts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        exclude = ['groups', 'user_permissions']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
