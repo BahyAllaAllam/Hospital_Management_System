24b3e929e95c2668e19a036a156acbab5e9f7fa8
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-16 23:02:07 +0200

    Fixing the routers order in doctors to get the department url

--- H_M_S/src/doctors/urls.py
+++ H_M_S/src/doctors/urls.py
@@ -3,8 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import DoctorViewSet, DepartmentViewSet
 
 router = DefaultRouter()
-router.register('', DoctorViewSet)
 router.register('departments', DepartmentViewSet)
+router.register('', DoctorViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
