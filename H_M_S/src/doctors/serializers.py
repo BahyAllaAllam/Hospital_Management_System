92f3491ba07e400b6e2e0da17ebadf5523590f49
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-16 22:27:04 +0200

    Creating serializers for doctors

---
+++ H_M_S/src/doctors/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Doctor, Department
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
