7e3989dfbb5767a046e49fa54a346f96bef68e87
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-16 22:29:45 +0200

    Creating doctors views

--- H_M_S/src/doctors/views.py
+++ H_M_S/src/doctors/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Doctor, Department
+from .serializers import DoctorSerializer, DepartmentSerializer
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [IsAuthenticated]
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = [IsAuthenticated]
