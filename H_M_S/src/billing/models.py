ba46f456ef0a63745d9b31a16e178baec4ab3f61
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-17 23:11:12 +0200

    add updated at to invoice

--- H_M_S/src/billing/models.py
+++ H_M_S/src/billing/models.py
@@ -14,6 +14,7 @@ class Invoice(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=10, choices=STATUS, default='unpaid')
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Invoice - {self.patient} - {self.total}"
