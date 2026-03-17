1cc98e6db97991246f8ec00b7d66b850b0d9b805
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-17 22:24:24 +0200

    Ceating urls for billing

--- H_M_S/src/H_M_S/urls.py
+++ H_M_S/src/H_M_S/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/patients/', include('patients.urls')),
     path('api/doctors/', include('doctors.urls')),
     path('api/appointments/', include('appointments.urls')),
+    path('api/billing/', include('billing.urls')),
 ]
