67d7b8f7d27bc814fd2b9d18708d083b913be5d5
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-17 22:22:17 +0200

    Creating views for billing

--- H_M_S/src/billing/views.py
+++ H_M_S/src/billing/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Invoice, InvoiceItem
+from .serializers import InvoiceSerializer, InvoiceItemSerializer
 
-# Create your views here.
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class InvoiceItemViewSet(viewsets.ModelViewSet):
+    queryset = InvoiceItem.objects.all()
+    serializer_class = InvoiceItemSerializer
+    permission_classes = [IsAuthenticated]
