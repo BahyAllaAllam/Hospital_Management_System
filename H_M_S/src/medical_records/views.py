70fd8224e8087b0115c08df95a5702b38f43542c
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-18 22:18:01 +0200

    Create mediacl records app views

--- H_M_S/src/medical_records/views.py
+++ H_M_S/src/medical_records/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import MedicalRecord, MedicalRecordAttachment
+from .serializers import MedicalRecordSerializer, MedicalRecordAttachmentSerializer
 
-# Create your views here.
+
+class MedicalRecordViewSet(viewsets.ModelViewSet):
+    queryset = MedicalRecord.objects.all()
+    serializer_class = MedicalRecordSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MedicalRecordAttachmentViewSet(viewsets.ModelViewSet):
+    queryset = MedicalRecordAttachment.objects.all()
+    serializer_class = MedicalRecordAttachmentSerializer
+    permission_classes = [IsAuthenticated]
