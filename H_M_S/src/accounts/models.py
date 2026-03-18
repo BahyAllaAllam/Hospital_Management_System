b9b5d3f332f7d4e7361854d8c7a895c1722b8ac6
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-18 23:02:10 +0200

    Adding new roles the accountant, radiology, lab and supervisor

--- H_M_S/src/accounts/models.py
+++ H_M_S/src/accounts/models.py
@@ -5,10 +5,14 @@ from django.db import models
 class User(AbstractUser):
     ROLES = (
         ('admin', 'Admin'),
+        ('supervisor', 'Supervisor'),
         ('doctor', 'Doctor'),
         ('nurse', 'Nurse'),
         ('receptionist', 'Receptionist'),
         ('patient', 'Patient'),
+        ('lab', 'Lab Technician'),
+        ('radiology', 'Radiology Technician'),
+        ('accountant', 'Accountant'),
     )
     role = models.CharField(max_length=20, choices=ROLES)
     phone = models.CharField(max_length=15, blank=True)
