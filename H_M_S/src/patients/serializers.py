80b96c951fb57e0d8733dcf95d2b854f1543f9c5
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-27 23:06:38 +0200

    Fix the response for the filter

--- H_M_S/src/patients/serializers.py
+++ H_M_S/src/patients/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
+from accounts.serializers import UserSerializer
 from .models import Patient
 
 class PatientSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+
+
     class Meta:
         model = Patient
         fields = '__all__'
