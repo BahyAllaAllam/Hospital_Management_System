cc3a1058997135e7ef754a6264ef2934d50c99c1
BahyAlaaAllam <bahy.alam@yahoo.com>
2026-03-31 01:57:15 +0200

    Add one orginized error response for all the project

---
+++ H_M_S/src/H_M_S/exceptions.py
@@ -0,0 +1,15 @@
+from rest_framework.views import exception_handler
+from rest_framework.response import Response
+
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+
+    if response is not None:
+        return Response({
+            'status': 'error',
+            'message': response.data.get('detail', str(response.data)),
+            'code': response.status_code
+        }, status=response.status_code)
+
+    return response
